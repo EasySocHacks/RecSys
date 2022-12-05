af949769c6ceaacd04c64af2381026c9504f55e7
Varfolomeev Alexander <vas.aleks.v@gmail.com>
2022-12-06 00:44:51 +0300

    Version w/o precalculating

--- service/rec_models/userknn.py
+++ service/rec_models/userknn.py
@@ -26,14 +26,17 @@ class Mappings:
         self.items_mapping: Dict[int, int] = items_mapping
 
 
-class PredCalculation:
-    def __init__(self,
-                 k_recs: int,
-                 known_users: Mapping,
-                 cold_user: List[int]):
-        self.k_recs = k_recs
-        self.known_users: Mapping = known_users
-        self.cold_user = cold_user
+class Watched:
+    def __init__(self, watched: pd.DataFrame):
+        self.watched: pd.DataFrame = watched
+        self.watched_dict: Mapping = watched['item_id'].to_dict()
+
+
+class IDF:
+    def __init__(self, item_idf: pd.DataFrame):
+        self.item_idf: pd.DataFrame = item_idf
+        self.item_idf_dict: Mapping = \
+            item_idf.set_index('index')['idf'].to_dict()
 
 
 class UserKnn(BaseRecModel):
@@ -47,12 +50,11 @@ class UserKnn(BaseRecModel):
 
         self.mappings: Optional[Mappings] = None
 
-        self.watched: Optional[pd.DataFrame] = None
-
         self.popular: PopularModel = PopularModel(verbose=1)
 
-        self.item_idf: Optional[pd.DataFrame] = None
-        self.pred_calc: Optional[PredCalculation] = None
+        self.dataset: Optional[Dataset] = None
+        self.watched: Optional[Watched] = None
+        self.item_idf: Optional[IDF] = None
 
     def get_mappings(self, train) -> None:
         users_inv_mapping = dict(enumerate(train['user_id'].unique()))
@@ -68,8 +70,6 @@ class UserKnn(BaseRecModel):
         )
 
     def get_matrix(self, df: pd.DataFrame,
-                   user_col: str = 'user_id',
-                   item_col: str = 'item_id',
                    weight_col: str = None) \
             -> sp.sparse.coo_matrix:
         if weight_col:
@@ -80,12 +80,12 @@ class UserKnn(BaseRecModel):
         interaction_matrix = sp.sparse.coo_matrix((
             weights,
             (
-                df[user_col].map(self.mappings.users_mapping.get),
-                df[item_col].map(self.mappings.items_mapping.get)
+                df['user_id'].map(self.mappings.users_mapping.get),
+                df['item_id'].map(self.mappings.items_mapping.get)
             )
         ))
 
-        self.watched = df.groupby(user_col).agg({item_col: list})
+        self.watched = Watched(df.groupby('user_id').agg({'item_id': list}))
         return interaction_matrix
 
     @staticmethod
@@ -103,7 +103,7 @@ class UserKnn(BaseRecModel):
                                           columns=['doc_freq']).reset_index()
         item_idf['idf'] = item_idf['doc_freq'].apply(
             lambda x: self.idf(n, x))
-        self.item_idf = item_idf
+        self.item_idf = IDF(item_idf)
 
     def fit(self, train: pd.DataFrame,
             save: bool = False, k_recs: int = 10) -> None:
@@ -114,21 +114,73 @@ class UserKnn(BaseRecModel):
 
         self.user_knn.fit(weights_matrix)
         #
-        dataset = Dataset.construct(
+        self.dataset = Dataset.construct(
             train,
         )
-        self.popular.fit(dataset)
+        self.popular.fit(self.dataset)
 
         self.is_fitted = True
-
-        unique_user_ids = train['user_id'].unique()
-        known_recos = self.recommend(unique_user_ids, dataset)
-        cold_user_recos = self._predict_popular(k_recs, dataset).tolist()
-        self.pred_calc = PredCalculation(k_recs, known_recos, cold_user_recos)
 
         if save:
             with open('dumps/userknn.dill', 'wb') as f:
                 dill.dump(self, f)
+
+    def _predict_userknn(self, user_id: int, k_recs: int) -> np.ndarray:
+        if user_id not in self.mappings.users_mapping:
+            return np.array([])
+
+        watched_by_user = set(self.watched.watched_dict[user_id])
+
+        int_user_id = self.mappings.users_mapping[user_id]
+        knn_recs = self.user_knn.similar_items(int_user_id, N=50)
+        knn_recs.sort(key=lambda t: t[1], reverse=True)
+        filtered_knn_recs: List[Tuple[int, float]] = []
+
+        def filter_func(x: int) -> bool:
+            return x not in watched_by_user
+
+        def add_idf(_sim: float) -> Callable[[int], Tuple[int, float]]:
+            return lambda x: (x, self.item_idf.item_idf_dict[x] * _sim)
+
+        for user, sim in knn_recs:
+            ext_user = self.mappings.users_inv_mapping[user]
+            if ext_user == user_id:
+                continue
+            watched = list(filter(
+                filter_func,
+                self.watched.watched_dict[ext_user]
+            ))
+            watched_by_user.update(watched)
+            filtered_knn_recs.extend(map(
+                add_idf(sim),
+                watched
+            ))
+
+        filtered_knn_recs.sort(key=lambda t: t[1], reverse=True)
+        return np.array(list(map(lambda t: t[0], filtered_knn_recs[:k_recs])))
+
+    def _predict_popular(self,
+                         k_recs: int
+                         ) -> np.ndarray:
+        popularity_list = self.popular.popularity_list
+
+        reco = popularity_list[0][:k_recs]
+
+        return self.dataset.item_id_map.convert_to_external(reco)
+
+    def predict(self, user_id: int, k_recs: int) -> List[int]:
+        if not self.is_fitted:
+            raise ValueError("Please call fit before predict")
+
+        if user_id not in self.mappings.users_mapping:
+            return self._predict_popular(k_recs).tolist()
+        watched = self.watched.watched_dict[user_id]
+
+        recs = self._predict_userknn(user_id, k_recs)
+
+        popular = self._predict_popular(k_recs + len(watched))
+
+        return np.unique(np.concatenate((recs, popular)))[:k_recs].tolist()
 
     def _generate_recs_mapper(self,
                               model: ItemItemRecommender,
@@ -143,71 +195,8 @@ class UserKnn(BaseRecModel):
 
         return _recs_mapper
 
-    def _predict_userknn(self, user_id: int) -> np.ndarray:
-        if user_id not in self.mappings.users_mapping:
-            return np.array([])
-        mapper = self._generate_recs_mapper(self.user_knn)
-
-        sim_users, sims = mapper(user_id)
-        recs = pd.DataFrame({'sim_user_id': sim_users, 'sim': sims})
-
-        recs = recs[recs['sim_user_id'] != user_id] \
-            .merge(self.watched,
-                   left_on=['sim_user_id'],
-                   right_on=['user_id'],
-                   how='left') \
-            .explode('item_id') \
-            .sort_values(['sim'], ascending=False) \
-            .drop_duplicates(['item_id'], keep='first') \
-            .merge(self.item_idf, left_on='item_id', right_on='index',
-                   how='left')
-
-        recs['score'] = recs['sim'] * recs['idf']
-        recs = recs.sort_values('score', ascending=False)
-
-        return recs['item_id'].to_numpy()
-
-    def _predict_popular(self,
-                         k_recs: int,
-                         dataset: Dataset
-                         ) -> np.ndarray:
-        popularity_list = self.popular.popularity_list
-
-        reco = popularity_list[0][:k_recs]
-
-        return dataset.item_id_map.convert_to_external(reco)
-
-    def predict(self, user_id: int, k_recs: int) -> List[int]:
-        if k_recs != self.pred_calc.k_recs:
-            raise ValueError("Invalid recommendation request count")
-
-        if user_id in self.pred_calc.known_users:
-            return np.unique(np.array(
-                self.pred_calc.known_users[user_id]
-                + self.pred_calc.cold_user
-            ))[:k_recs].tolist()
-        return self.pred_calc.cold_user
-
-    # def _generate_predict_mapper(self, dataset: Dataset, k_recs: int) \
-    #         -> Callable[[int], List[int]]:
-    #     if not self.is_fitted:
-    #         raise ValueError("Please call fit before predict")
-    #
-    #     def predict_mapper(user_id: int) -> List[int]:
-    #         if user_id not in self.mappings.users_mapping:
-    #             return self._predict_popular(k_recs, dataset).tolist()
-    #         watched = self.watched['item_id'][user_id]
-    #
-    #         recs = self._predict_userknn(user_id)
-    #
-    #       popular = self._predict_popular(2 * k_recs + len(watched), dataset)
-    #
-    #       return np.unique(np.concatenate((recs, popular)))[:k_recs].tolist()
-    #     return predict_mapper
-
     def recommend(self,
-                  test: np.ndarray,
-                  dataset: Dataset,
+                  test: pd.DataFrame,
                   k_recs: int = 10) -> Mapping:
         if not self.is_fitted:
             raise ValueError("Please call fit before predict")
@@ -216,19 +205,21 @@ class UserKnn(BaseRecModel):
 
         mapper = self._generate_recs_mapper(self.user_knn)
 
-        recs = pd.DataFrame({'user_id': test})
+        recs = pd.DataFrame({'user_id': test['user_id']})
         recs['sim_user_id'], recs['sim'] = zip(*recs['user_id'].map(mapper))
         recs = recs.set_index('user_id').apply(pd.Series.explode).reset_index()
 
         print(1)
 
         recs = recs[~(recs['sim'] >= 0.999999)] \
-            .merge(self.watched, left_on=['sim_user_id'], right_on=['user_id'],
+            .merge(self.watched.watched,
+                   left_on=['sim_user_id'],
+                   right_on=['user_id'],
                    how='left') \
             .explode('item_id') \
             .sort_values(['user_id', 'sim'], ascending=False) \
             .drop_duplicates(['user_id', 'item_id'], keep='first') \
-            .merge(self.item_idf, left_on='item_id', right_on='index',
+            .merge(self.item_idf.item_idf, left_on='item_id', right_on='index',
                    how='left')
         #   .groupby(['user_id', 'sim']).agg(UserKnn.momentum) \
 
@@ -237,20 +228,23 @@ class UserKnn(BaseRecModel):
         recs['score'] = recs['sim'] * recs['idf']
         recs = recs.sort_values(['user_id', 'score'], ascending=False)
 
-        popular = self.popular.recommend(test, dataset, k_recs, True)
+        popular = self.popular.recommend(
+            test['user_id'].unique(),
+            self.dataset,
+            k_recs,
+            True
+        )
         recs = pd.concat([
-            recs[['user_id', 'item_id']],
-            popular[['user_id', 'item_id']],
+            recs[['user_id', 'item_id', 'score']],
+            popular[['user_id', 'item_id', 'score']],
         ], ignore_index=True).reset_index()
 
         print(3)
 
-        recs['rank'] = recs.groupby('user_id').cumcount()
+        recs['rank'] = recs.groupby('user_id').cumcount() + 1
 
-        res = recs[recs['rank'] <= k_recs] \
-            .groupby('user_id') \
-            .agg({'item_id': list})['item_id'] \
-            .to_dict()
+        res = recs[recs['rank'] <= k_recs][
+            ['user_id', 'item_id', 'score', 'rank']]
 
         print(4)
 
