4c994f3b4e986cbefbbd471ae18b5193e31f7aba
EasySocHacks <easy.soc.hacks@gmail.com>
2022-12-07 14:05:31 +0300

    append model to api

--- service/rec_models/__init__.py
+++ service/rec_models/__init__.py
@@ -1,8 +1,12 @@
 from typing import Dict
 
+from .als_model import ALSModel
 from .base_model import BaseRecModel
+from .factoring_machine_model import FactoringMachineModel
 from .test_model import TestModel
 
 modelByName: Dict[str, BaseRecModel] = {
-    "test_model": TestModel()
+    "test_model": TestModel(),
+    "als": ALSModel.load(),
+    "factoring_machine": FactoringMachineModel.load()
 }
