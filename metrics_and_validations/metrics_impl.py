d8d05354fe7c773c33f6e4b32bfde14a0073e1bd
ReachPeach <maxim.busyuk@gmail.com>
2022-12-10 00:48:04 +0300

    Migrated to 2D graphics & extended k parameters via review

--- metrics_and_validations/metrics_impl.py
+++ metrics_and_validations/metrics_impl.py
@@ -10,33 +10,34 @@ def plook(ind, rels) -> float:
     return plook(ind - 1, rels) * (1 - rels[ind - 1]) * (1 - 0.15)
 
 
-def pfound(group):
+def pfound(group, k=10):
     # максимальный рейтинг хоста
     max_by_host = group.groupby("hostid")["rating"].max()
-    # берем топ10 урлов с наивысшим рейтингом
-    top10 = max_by_host.sort_values(ascending=False).iloc[:10]
+    # берем топ-k урлов с наивысшим рейтингом
+    top_k = max_by_host.sort_values(ascending=False).iloc[:k]
     pfound = 0.
-    for ind, val in enumerate(top10):
-        pfound += val * plook(ind, top10.values)
+    for ind, val in enumerate(top_k):
+        pfound += val * plook(ind, top_k.values)
     return pfound
 
 
-def pfound_fast(group):
+def pfound_fast(group, k=10):
     max_by_host = group.groupby("hostid")["rating"].max().to_numpy()
-    top10 = np.flip(np.sort(max_by_host))[:10]
+    top_k = np.flip(np.sort(max_by_host))[:k]
 
-    plook_vals = (np.ones(10) - top10) * (1 - 0.15)
-    plook_vals = np.append(np.ones(1), plook_vals).cumprod()[:10]
+    plook_vals = (np.ones(k) - top_k) * (1 - 0.15)
+    plook_vals = np.append(np.ones(1), plook_vals).cumprod()[:k]
 
-    return np.dot(plook_vals, top10)
+    return np.dot(plook_vals, top_k)
 
 
-def mrr_naive(users, target, recs):
+def mrr_naive(users, target, recs, k):
     mrr = [0. for _ in users]
+    used_recs = recs[:, :k]
     for i, user in enumerate(users):
         rr = 0
         user_target = target[target[:, 0] == user][:, 1]
-        for rank, rec in enumerate(recs[i]):
+        for rank, rec in enumerate(used_recs[i]):
             if rec in user_target:
                 rr = rank + 1
                 break
@@ -46,13 +47,14 @@ def mrr_naive(users, target, recs):
 
 
 @nb.njit(cache=True, parallel=True)
-def mrr_numba(users, target, recs):
+def mrr_numba(users, target, recs, k):
     rr = np.zeros(len(users))
+    used_recs = recs[:, :k]
     for i in nb.prange(len(rr)):
         user = users[i]
         p = 0.
         user_target = target[target[:, 0] == user][:, 1]
-        for ind, rec in enumerate(recs[i]):
+        for ind, rec in enumerate(used_recs[i]):
             if rec in user_target:
                 p = 1. / (ind + 1.)
                 break
@@ -60,11 +62,11 @@ def mrr_numba(users, target, recs):
     return sum(rr) / len(rr)
 
 
-def mrr_pandas(users, df, recs):
-    k = recs.shape[1]
+def mrr_pandas(users, df, recs, k):
+    used_recs = recs[:, :k]
     df_recs = pd.DataFrame({
         Columns.User: np.repeat(users, k),
-        Columns.Item: recs.ravel()
+        Columns.Item: used_recs.ravel()
     })
     df_recs[Columns.Rank] = df_recs.groupby(Columns.User).cumcount() + 1
     df_recs = df.merge(df_recs, how='inner', left_on=Columns.UserItem,
