ae951baea838a374098bf7d2d94a95a508ba2f40
Varfolomeev Alexander <vas.aleks.v@gmail.com>
2022-12-09 20:21:05 +0300

    dump file name change

--- service/rec_models/__init__.py
+++ service/rec_models/__init__.py
@@ -8,7 +8,7 @@ from .userknn import UserKnn
 modelByName: Dict[str, BaseRecModel] = {
     "test_model": TestModel(),
     "userknn_with_popularity":
-        UserKnn.load("userknn_tfidf.dill")
-        if exists("dumps/userknn_tfidf.dill")
+        UserKnn.load("userknn.dill")
+        if exists("dumps/userknn.dill")
         else None,
 }
