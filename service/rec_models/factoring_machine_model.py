a2a7cfe8e21f618f46bea8eb7044eb8215977301
EasySocHacks <easy.soc.hacks@gmail.com>
2022-12-11 01:25:24 +0300

    fix lint error

--- service/rec_models/factoring_machine_model.py
+++ service/rec_models/factoring_machine_model.py
@@ -1,8 +1,10 @@
+import os
 from typing import Any, List, Tuple, Union
 
 import numpy as np
 import pandas as pd
 from lightfm import LightFM
+from rectools import Columns
 from rectools.dataset import Dataset
 from rectools.models import LightFMWrapperModel
 
@@ -10,47 +12,151 @@ from service.rec_models import BaseRecModel
 from service.rec_models.exceptions import RecModelNotLearnedYetException
 
 _MODEL_NAME = "FactoringMachine"
+ROOT_DIR = os.path.abspath(os.curdir)
+
+
+class FMHyperParams:
+    def __init__(
+        self,
+        n_factors: int,
+        loss: str,
+        lr: float,
+        ua: int,
+        ia: int,
+    ):
+        self.n_factors: int = n_factors
+        self.loss: str = loss
+        self.lr: float = lr
+        self.ua: int = ua
+        self.ia: int = ia
+
+
+class FMTuneParams:
+    def __init__(
+        self,
+        n_epoch: int = 1,
+        n_threads: int = 1,
+        seed: int = 0,
+    ):
+        self.n_epoch: int = n_epoch
+        self.n_threads: int = n_threads
+        self.seed: int = seed
 
 
 class FactoringMachineModel(BaseRecModel):
     def __init__(
         self,
         dataset: Union[Dataset, pd.DataFrame],
-        n_factors: int,
-        loss: str,
-        lr: float,
-        ua: int,
-        ia: int,
-        n_epoch: int = 1,
-        n_threads: int = 1,
-        seed: int = 0,
+        hyper_params: FMHyperParams,
+        tune_params: FMTuneParams,
     ):
-        self._n_factors: int = n_factors
-        self._loss: str = loss
-        self._lr: float = lr
-        self._ua: int = ua
-        self._ia: int = ia
-        self._n_epoch: int = n_epoch
-        self._n_threads: int = n_threads
-        self._seed: int = seed
+        self.dataset = dataset
+
+        self._hyper_params = hyper_params
+        self._tune_params = tune_params
 
         self.user_embeddings: Any = None
         self.item_embeddings: Any = None
 
         self.model = LightFMWrapperModel(
             LightFM(
-                no_components=self._n_factors,
-                loss=self._loss,
-                random_state=self._seed,
-                learning_rate=self._lr,
-                user_alpha=self._ua,
-                item_alpha=self._ia,
+                no_components=self._hyper_params.n_factors,
+                loss=self._hyper_params.loss,
+                random_state=self._tune_params.seed,
+                learning_rate=self._hyper_params.lr,
+                user_alpha=self._hyper_params.ua,
+                item_alpha=self._hyper_params.ia,
             ),
-            epochs=self._n_epoch,
-            num_threads=self._n_threads,
+            epochs=self._tune_params.n_epoch,
+            num_threads=self._tune_params.n_threads,
         )
 
-        self.fit(dataset)
+        self.fit(self.dataset)
+
+    @staticmethod
+    def prepare():
+        os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
+        interactions = pd.read_csv(f'{ROOT_DIR}/kion_train/interactions.csv')
+        users = pd.read_csv(f'{ROOT_DIR}/kion_train/users.csv')
+        items = pd.read_csv(f'{ROOT_DIR}/kion_train/items.csv')
+
+        cold_users = list(set(users["user_id"]) - set(interactions["user_id"]))
+        cold_users.append(0)
+        data = {
+            "user_id": cold_users,
+            "item_id": [0] * len(cold_users),
+            "last_watch_dt": "1990-01-01",
+            "total_dur": [0] * len(cold_users),
+            "watched_pct": [0] * len(cold_users)
+        }
+        new_interactions = pd.DataFrame(data)
+        interactions = interactions.append(new_interactions)
+
+        cold_items = list(set(items["item_id"]) - set(interactions["item_id"]))
+        cold_items.append(0)
+        data = {
+            "user_id": [0] * len(cold_items),
+            "item_id": cold_items,
+            "last_watch_dt": "1990-01-01",
+            "total_dur": [0] * len(cold_items),
+            "watched_pct": [0] * len(cold_items)
+        }
+        new_interactions = pd.DataFrame(data)
+        interactions = interactions.append(new_interactions)
+
+        interactions[Columns.Weight] = \
+            np.where(interactions['watched_pct'] > 10, 3, 1)
+        interactions[Columns.Weight].value_counts(normalize=True)
+
+        Columns.Datetime = 'last_watch_dt'
+
+        interactions[Columns.Datetime] = pd.to_datetime(
+            interactions[Columns.Datetime], format='%Y-%m-%d')
+
+        user_features_frames = []
+        for feature in ["sex", "age", "income"]:
+            feature_frame = users.reindex(columns=[Columns.User, feature])
+            feature_frame.columns = ["id", "value"]
+            feature_frame["feature"] = feature
+            user_features_frames.append(feature_frame)
+        user_features = pd.concat(user_features_frames)
+
+        items["genre"] = items["genres"].str.lower() \
+            .str.replace(", ", ",", regex=False).str.split(",")
+        genre_feature = items[["item_id", "genre"]].explode("genre")
+        genre_feature.columns = ["id", "value"]
+        genre_feature["feature"] = "genre"
+
+        content_feature = items.reindex(columns=[Columns.Item, "content_type"])
+        content_feature.columns = ["id", "value"]
+        content_feature["feature"] = "content_type"
+
+        item_features = pd.concat((genre_feature, content_feature))
+
+        dataset = Dataset.construct(
+            interactions_df=interactions,
+            user_features_df=user_features,
+            cat_user_features=["sex", "age", "income"],
+            item_features_df=item_features,
+            cat_item_features=["genre", "content_type"],
+        )
+
+        return FactoringMachineModel(
+            dataset,
+            FMHyperParams(
+                64,
+                "warp",
+                0.01,
+                0,
+                0,
+            ),
+            FMTuneParams(
+                1,
+                16,
+                123,
+            ),
+        )
 
     @staticmethod
     def _augment_inner_product(factors: int) -> Tuple[float, np.ndarray]:
@@ -63,15 +169,12 @@ class FactoringMachineModel(BaseRecModel):
         return max_norm, augmented_factors
 
     def recommend(self, user_id: int, k_recs: int) -> List[int]:
-        try:
-            labels, _ = self._recommend_all(
-                self.user_embeddings[[user_id], :],
-                self.item_embeddings
-            )
+        labels, _ = self._recommend_all(
+            self.user_embeddings[[user_id], :],
+            self.item_embeddings
+        )
 
-            return list(labels.flatten())
-        except Exception:
-            return list(range(k_recs))
+        return list(labels.flatten())
 
     @staticmethod
     def _recommend_all(
